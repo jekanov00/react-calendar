439a19ccdc8cc070d7f78cd0bf415529df07781a
Eugene Samoylenko <evgeniysamoylenko2000@gmail.com>
2020-09-04 20:25:45 +0300

    refactor reset.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import './assets/reset.module.css/reset.module.css';
+import './assets/css/reset.css';
 
 import Calendar from './components/Calendar/Calendar';
 
